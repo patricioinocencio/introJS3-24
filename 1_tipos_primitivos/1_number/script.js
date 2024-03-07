63ed93ffd64a0c4dc93cef4fab4f83143e77b5ed
patricio inocencio <126088130+patricioinocencio@users.noreply.github.com>
2024-03-06 22:29:11 -0300

    Add files via upload

---
+++ 1_tipos_primitivos/1_number/script.js
@@ -0,0 +1,17 @@
+//tipo number
+
+const meuNumero = 57;
+
+const telefone = 44999999999;
+
+const idade = 18;
+
+
+console.log(meuNumero);
+console.log(telefone);
+console.log(idade);
+
+
+const soma = meuNumero + idade;
+
+console.log(soma);
